a3fc12f398c3c4f4384c7fa8896554ca284850fd
xsd1397 <xiesd55913@outlook.com>
2026-09-02 07:08:03 +0800

    ci: 同步全局 API 配置到 main 分支

---
+++ client/src/const.ts
@@ -0,0 +1,9 @@
+﻿export const getBackendApiUrl = (): string => {
+  if (typeof process !== "undefined" && process.env?.EXPO_PUBLIC_API_URL) {
+    return process.env.EXPO_PUBLIC_API_URL;
+  }
+  if (typeof process !== "undefined" && process.env?.VITE_BACKEND_URL) {
+    return process.env.VITE_BACKEND_URL;
+  }
+  return "https://speakwise-wsicpu2u.manus.space";
+};
